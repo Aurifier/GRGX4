4be49858c80ca4f6981017dec5891db132e4fbfc
Aurifier <reed.995@osu.edu>
2014-06-24 12:09:29 -0400

    Fixed invalid karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'js/*.js',
+      'src/*.js',
       'test/*.js'
     ],
 
@@ -29,7 +29,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        ['coverage']
+        'src/*.js': ['coverage']
     },
 
 
