4d49024aee61c2717ad5d61b2338131468cfdd57
Aurifier <reed.995@osu.edu>
2014-07-01 14:15:38 -0400

    Refactor for readability and theoretical performance.

--- src/Searcher.js
+++ src/Searcher.js
@@ -6,26 +6,37 @@ Searcher.prototype.getSpeciesList = function() {
     return this.speciesList;
 };
 
-//TODO: Refactor to parallelize JSON queries
 Searcher.prototype.find = function(term) {
     var self = this;
     return new Promise(function(resolve, reject) {
-        var url = '/' + self.speciesList + '/gene/json';
-        $.getJSON(url, {name: term}).then( function(geneJSON) {
-            var resolveList = [];
-            if(geneJSON.length == 1) {
-                var exactGene = {id:geneJSON[0].id, name: geneJSON[0].name};
-                resolveList.push(exactGene);
-            }
+        var exactGenePromise = new Promise(function(gResolve, gReject) {
+            var url = '/' + self.speciesList + '/gene/json';
+            $.getJSON(url, {name: term}).then( function(geneJSON) {
+                if(geneJSON.length == 1) {
+                    var exactGene = {id:geneJSON[0].id, name: geneJSON[0].name};
+                    gResolve(exactGene);
+                }
+            });
+        });
 
-            url = '/' + self.speciesList + '/protein/json';
+        var exactProteinPromise = new Promise(function(pResolve, pReject) {
+            var url = '/' + self.speciesList + '/protein/json';
             $.getJSON(url, {name: term}).then( function(protJSON) {
                 if(protJSON.length == 1) {
                     var exactProt = {id: protJSON[0].id, name: protJSON[0].name};
-                    resolveList.push(exactProt);
+                    pResolve(exactProt);
+                } else {
+                    pResolve(null);
                 }
-                resolve(resolveList);
             });
+        });
+
+        function isNotNull(val) {
+           return(val != null);
+        }
+
+        Promise.all([exactGenePromise, exactProteinPromise]).then(function(matches) {
+            resolve(matches.filter(isNotNull));
         });
     });
 };
