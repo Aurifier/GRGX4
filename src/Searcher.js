e326590893182fb3bf13b84fcb663d4def68db5b
Aurifier <reed.995@osu.edu>
2014-07-11 11:51:47 -0400

    Move some functions around.

--- src/Searcher.js
+++ src/Searcher.js
@@ -12,45 +12,11 @@ Searcher.prototype.find = function(term) {
         var pExactGenes = [];
         var pExactProteins = [];
 
-        var exactCallback = function(eRes, eRej, species) {
-            return(function(exactJSON) {
-                if(exactJSON.length == 1) {
-                    var exactObj = {species: species, id:exactJSON[0].id, name: exactJSON[0].name};
-                    eRes(exactObj);
-                } else {
-                    eRes(null);
-                }
-            });
-        };
-
-        //TODO: There is even more room for refactoring between these two functions.
-        var exactGenePromise = function(species) {
-            return(function(res, rej) {
-                var url = '/' + species + '/gene/json';
-                $.getJSON(url, {name: term}).then(exactCallback(res, rej, species));
-            });
-        };
-
-        var exactProteinPromise = function(species) {
-            return(function(res, rej) {
-                var url = '/' + species + '/protein/json';
-                $.getJSON(url, {name: term}).then(exactCallback(res, rej, species));
-            });
-        };
-
         for(var i = 0; i < self.speciesList.length; i++) {
             var species = self.speciesList[i];
 
             pExactGenes.push(new Promise(exactGenePromise(species)));
             pExactProteins.push(new Promise(exactProteinPromise(species)));
-        }
-
-        function isNotNull(val) {
-           return(val !== null);
-        }
-
-        function nonEmptyResult(result) {
-            return(Boolean(result.results));
         }
 
         Promise.all(pExactGenes.concat(pExactProteins)).then(function(matches) {
@@ -68,5 +34,38 @@ Searcher.prototype.find = function(term) {
             });
             resolve(results.filter(nonEmptyResult));
         });
+
+        function exactGenePromise(species) {
+            return(function(res, rej) {
+                var url = '/' + species + '/gene/json';
+                $.getJSON(url, {name: term}).then(exactCallback(res, rej, species));
+            });
+        }
+
+        function exactProteinPromise(species) {
+            return(function(res, rej) {
+                var url = '/' + species + '/protein/json';
+                $.getJSON(url, {name: term}).then(exactCallback(res, rej, species));
+            });
+        }
+
+        function exactCallback(eRes, eRej, species) {
+            return(function(exactJSON) {
+                if(exactJSON.length == 1) {
+                    var exactObj = {species: species, id:exactJSON[0].id, name: exactJSON[0].name};
+                    eRes(exactObj);
+                } else {
+                    eRes(null);
+                }
+            });
+        }
+
+        function isNotNull(val) {
+           return(val !== null);
+        }
+
+        function nonEmptyResult(result) {
+            return(Boolean(result.results));
+        }
     });
 };
