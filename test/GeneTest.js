58f46a7f8e7cd076e5049d1c84a1aacc91551ef0
Aurifier <reed.995@osu.edu>
2014-07-22 09:57:33 -0400

    Merge tests.

--- test/GeneTest.js
+++ test/GeneTest.js
@@ -1,21 +1,29 @@
 describe("A gene", function() {
-    it("should have an id and species when provided", function() {
-        var id = 153;
-        var species = "urksbar";
-
-        var imaGene = new Gene({id: id, species: species});
-
-        expect(imaGene.id).toEqual(id);
-        expect(imaGene.species).toEqual(species);
-    });
-
-    it("should have a name when provided", function() {
+    it("should have an id, name, and species when provided", function() {
         var id = 54;
         var species = "urknawa";
         var name = "UnPudu823";
 
         var imaGene = new Gene({id: id, species: species, name: name});
 
-        expect(imaGene.getName()).toEqual(name);
+        expect(imaGene.id).toEqual(id);
+        expect(imaGene.name).toEqual(name);
+        expect(imaGene.species).toEqual(species);
+    });
+
+    xit("should fetch its name when not provided", function() {
+        var id = 45;
+        var species = "kooloolimpah";
+        var expectedName = "KllAch45";
+        jasmine.Ajax.stubRequest('/'+species+'/gene/'+id+'/json').andReturn({
+            "status": 200,
+            "contentType": "application/json",
+            "responseText": '[{"id":' + id + ',"name":"' + expectedName + '","type":"","ranges":"","transcript":""}]'
+        });
+
+        var imaGene = new Gene({id: id, species: species});
+        console.log(imaGene.ready);
+
+        expect(imaGene.name).toEqual(expectedName);
     });
 });
