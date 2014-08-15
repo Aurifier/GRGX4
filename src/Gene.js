cd51c3d3b2bfefd5badb2226e30a603619ebc6bb
Aurifier <reed.995@osu.edu>
2014-08-15 14:28:04 -0400

    More Gene documentation.

--- src/Gene.js
+++ src/Gene.js
@@ -1,5 +1,9 @@
 /**
  * Represents a Gene.
+ *
+ * In most cases, you should use `Gene.getGene` to instantiate a new
+ * Gene, unless you already have all of the properties and just want to collect
+ * them into an object.
  * @class Gene
  * @param cArgs
  * @param {Integer} cArgs.id
@@ -8,8 +12,25 @@
  * @constructor
  */
 function Gene(cArgs) {
+    /**
+     * The id of the Gene.
+     * @property id
+     * @type Integer
+     */
     this.id = cArgs.id;
+
+    /**
+     * The species of the Gene.
+     * @property species
+     * @type String
+     */
     this.species = cArgs.species;
+
+    /**
+     * The name of the Gene.
+     * @property name
+     * @type String
+     */
     this.name = cArgs.name;
 }
 
