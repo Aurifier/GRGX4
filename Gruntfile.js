a84fbdaf88917db37a4d5e60cef42a7a577082c6
Aurifier <reed.995@osu.edu>
2014-12-18 13:14:59 -0500

    More automation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,13 @@
-//TODO: Lint yuidoc
 module.exports = function(grunt) {
- 
+  var globalConfig = {
+      src: 'src/'
+  };
+
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    globalConfig: globalConfig,
     jshint: {
-      all: ['src/*.js']
+      all: ['<%= globalConfig.src %>*.js']
     },
     karma: {
       unit: {
@@ -15,10 +19,42 @@ module.exports = function(grunt) {
         singleRun: true
         browsers: ['PhantomJS']
       }*/
+    },
+    yuidoc: {
+      compile: {
+        name: '<%= pkg.name %>',
+        description: '<%= pkg.description %>',
+        version: '<%= pkg.version %>',
+        url: '<%= pkg.homepage %>',
+        options: {
+          paths: '<%= globalConfig.src %>',
+          //themedir: 'path/to/custom/theme/',
+          outdir: 'docs/'
+        }
+      }
     }
   });
  
+  grunt.registerTask('yuidoc-lint', 'Lint YUIDocs', function() {
+      var done = this.async();
+      testlint = grunt.util.spawn({
+          cmd: './node_modules/.bin/yuidoc',
+          args: ['--lint', globalConfig.src]
+      }, function(error, result, code) {
+          if(code == 1) {
+              return grunt.log.error('YUIDoc lint failed');
+          }
+          done(error);
+      });
+      testlint.stdout.pipe(process.stdout);
+      testlint.stderr.pipe(process.stderr);
+  });
+
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-karma');
-  grunt.registerTask('default', ['jshint','karma']);
+  grunt.loadNpmTasks('grunt-contrib-yuidoc');
+
+  grunt.registerTask('lint', ['jshint', 'yuidoc-lint']);
+  grunt.registerTask('all', ['lint', 'karma', 'yuidoc']);
+  grunt.registerTask('default', ['all']);
 };
