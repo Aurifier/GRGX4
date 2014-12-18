ce7365582332b2ad672b5fd828fd4460ab614437
Aurifier <reed.995@osu.edu>
2014-12-18 11:44:29 -0500

    Preparing for CI (hopefully).

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,21 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       all: ['src/*.js']
+    },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js',
+        singleRun: true
+      }
+      /*continuous: { //PhantomJS doesn't support Promises yet
+        configFile: 'karma.conf.js',
+        singleRun: true
+        browsers: ['PhantomJS']
+      }*/
     }
   });
  
   grunt.loadNpmTasks('grunt-contrib-jshint');
- 
-  grunt.registerTask('default', ['jshint']);
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.registerTask('default', ['jshint','karma']);
 };
