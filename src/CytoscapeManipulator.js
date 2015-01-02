7d9747177b3904d624a5c54d1c6080b2a245cd64
Aurifier <reed.995@osu.edu>
2015-01-02 11:15:50 -0500

    Documentation for CytoscapeManipulator.

--- src/CytoscapeManipulator.js
+++ src/CytoscapeManipulator.js
@@ -1,16 +1,25 @@
-//TODO: Documentation
+/**
+ * Wraps a Cytoscape.js object to easily add Interaction objects.
+ * @class CytoscapeManipulator
+ * @param myCy The Cytoscape.js object to wrap.
+ * @constructor
+ */
 function CytoscapeManipulator(myCy) {
     this.cy = myCy;
 }
 
-CytoscapeManipulator.prototype.add = function(obj) {
+/**
+ * Add an interaction to Cytoscape.js
+ * @method add
+ * @param {Interaction} interaction The Interaction object to add to Cytoscape.js */
+CytoscapeManipulator.prototype.add = function(interaction) {
     var nodes = [];
-    var target = {data: {id: obj.target.id, name: obj.target.name}};
+    var target = {data: {id: interaction.target.id, name: interaction.target.name}};
     nodes.push(target);
-    var source = Retriever.fetchProteinGroup(obj.source)[0];
+    var source = Retriever.fetchProteinGroup(interaction.source)[0];
     source = {data: {id: source.id, name: source.name}};
     nodes.push(source);
-    var edgeId = obj.source + "_" + obj.type + "_" + obj.target.id;
+    var edgeId = interaction.source + "_" + interaction.type + "_" + interaction.target.id;
     var edge = {data: {id: edgeId, source: source.data.id, target: target.data.id}};
     this.cy.add({nodes:nodes, edges:[edge]});
 };
