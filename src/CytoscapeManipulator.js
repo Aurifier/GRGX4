572a2cc337066da826246b669595933bf34404be
Aurifier <reed.995@osu.edu>
2015-01-02 11:29:25 -0500

    Refactor CytoscapeManipulator

--- src/CytoscapeManipulator.js
+++ src/CytoscapeManipulator.js
@@ -14,13 +14,13 @@ function CytoscapeManipulator(myCy) {
  * @param {Interaction} interaction The Interaction object to add to Cytoscape.js
  */
 CytoscapeManipulator.prototype.add = function(interaction) {
-    var nodes = [];
-    var target = {data: {id: interaction.target.id, name: interaction.target.name}};
-    nodes.push(target);
-    var source = Retriever.fetchProteinGroup(interaction.source)[0];
-    source = {data: {id: source.id, name: source.name}};
-    nodes.push(source);
+    var target = geneOrProteinToNode(interaction.target);
+    var source = geneOrProteinToNode(Retriever.fetchProteinGroup(interaction.source)[0]);
     var edgeId = interaction.source + "_" + interaction.type + "_" + interaction.target.id;
     var edge = {data: {id: edgeId, source: source.data.id, target: target.data.id}};
-    this.cy.add({nodes:nodes, edges:[edge]});
+    this.cy.add({nodes:[target, source], edges:[edge]});
 };
+
+function geneOrProteinToNode(obj) {
+    return {data: {id: obj.id, name: obj.name}};
+}
