0f850b8ed6564dce055159192a83dab0cb7185eb
Aurifier <reed.995@osu.edu>
2015-01-15 16:12:04 -0500

    Remove unnecessary tests.

--- test/CytoscapeManipulatorTest.js
+++ test/CytoscapeManipulatorTest.js
@@ -10,32 +10,6 @@ describe("The CytoscapeManipulator", function() {
         };
     });
 
-    //Doesn't need to do this.
-    xit("should add a single Gene object to the network", function() {
-        spyOn(mockCy, 'add');
-        var geneId = 654;
-        var name = "barfunkel";
-        var mockGene = {id: geneId, name: name};
-
-        var manipulator = new CytoscapeManipulator(mockCy);
-        manipulator.add(mockGene);
-
-        expect(mockCy.add).toHaveBeenCalledWith({group: 'nodes', data: {id: geneId, name: name}});
-    });
-
-    //Doesn't need to do this.
-    xit("should add a second Gene object to the network", function() {
-        spyOn(mockCy, 'add');
-        var geneId = 18;
-        var name = 'Rumpelstiltskin';
-        var mockGene = {id: geneId, name: name};
-
-        var manipulator = new CytoscapeManipulator(mockCy);
-        manipulator.add(mockGene);
-
-        expect(mockCy.add).toHaveBeenCalledWith({group: 'nodes', data: {id: geneId, name: name}});
-    });
-
     it("should add a simple interaction to the network", function() {
         var geneId = 54;
         var geneName = 'Display This Text';
